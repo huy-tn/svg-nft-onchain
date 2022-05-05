b156ae74e1608cc83100ffdb29e21dc48d7fe19f
huythai <dangerous235@gmail.com>
2022-05-05 17:15:11 +0700

    fix: get accounts in deploy random svg

--- deploy/00_deploy_mock.js
+++ deploy/00_deploy_mock.js
@@ -1,26 +1,24 @@
-module.exports = async ({
-    getNamedAccounts,
-    deployments,
-    getChainId
-}) => {
-    const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts()
-    const chainId = await getChainId()
-    log(chainId)
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = await getChainId();
+    log(chainId);
 
     if (chainId == 31337) {
-        log("Local network detected! Deploying Mocks...")
+        log("Local network detected! Deploying Mocks...");
 
-        const linkToken = await deploy('LinkToken', {from: deployer, log: true})
-        const vrfCoordinator = await deploy('VRFCoordinatorMock', {
-            from: deployer, 
+        const linkToken = await deploy("LinkToken", {
+            from: deployer,
             log: true,
-            args: [linkToken.address]
-        })
+        });
+        const vrfCoordinator = await deploy("VRFCoordinatorMock", {
+            from: deployer,
+            log: true,
+            args: [linkToken.address],
+        });
 
-        log("Mocks deployed")
+        log("Mocks deployed");
     }
+};
 
-}
-
-module.exports.tags = ['all', 'rsvg']
+module.exports.tags = ["all", "rsvg", "rsvgx"];
--- deploy/02_deploy_rsvg.js
+++ deploy/02_deploy_rsvg.js
@@ -1,4 +1,4 @@
-let { networkConfig } = require("../helper-hardhat-config");
+let { networkConfig, getNetworkIdFromName } = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     const { deploy, get, log } = deployments;
@@ -28,21 +28,30 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
     const RandomSVG = await deploy("RandomSVG", {
         from: deployer,
-        log: true,
         args: args,
+        log: true,
     });
-    log("Deployed RandomSVG");
-    networkName = networkConfig[chainId]["name"];
+    log(`Deployed RandomSVG at ${RandomSVG.address}`);
+    const networkName = networkConfig[chainId]["name"];
     log(
         `Verify with npx hardhat ${networkName} ${RandomSVG.address} ${args
             .toString()
             .replace(/,/g, " ")}`
     );
+    const RandomSVGContract = await ethers.getContractFactory("RandomSVG");
+    const accounts = await hre.ethers.getSigners();
+    const signer = accounts[0];
+    const randomSVG = new ethers.Contract(
+        RandomSVG.address,
+        RandomSVGContract.interface,
+        signer
+    );
 
     // fund with LINK
+    let networkId = await getNetworkIdFromName(network.name);
+    const fundAmount = networkConfig[networkId]["fundAmount"];
     const linkTokenContract = await ethers.getContractFactory("LinkToken");
-    const accounts = hre.ethers.getSigners();
-    const signer = accounts[0];
+
     const linkToken = new ethers.Contract(
         linkTokenAddress,
         linkTokenContract.interface,
@@ -52,7 +61,33 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     let fund_tx = await linkToken.transfer(RandomSVG.address, fee);
     await fund_tx.wait(1);
 
-    const RandomSVGContract = await ethers.getContractFactory("RandomSVG");
-    const RandomSVG = new ethers.Contract();
+
+    let creation_tx = await randomSVG.create({ gasLimit: 300000 });
+    let receipt = await creation_tx.wait(1);
+    let tokenId = receipt.events[3].topics[2];
+    log(`You've made NFT with id ${tokenId.toString()}`);
+    log(`Wait for ChainLink`);
+
+    if (chainId != 31337) {
+    } else {
+        const VRFCoordinatorMock = await deployments.get("VRFCoordinatorMock");
+        vrfCoordinator = await ethers.getContractAt(
+            "VRFCoordinatorMock",
+            VRFCoordinatorMock.address,
+            signer
+        );
+        let vrf_tx = await vrfCoordinator.callBackWithRandomness(
+            receipt.logs[3].topics[1],
+            77777,
+            randomSVG.address
+        );
+        await vrf_tx.wait(1);
+        log(`Now finish minting process`);
+        let finish_tx = await randomSVG.finishMint(tokenId, {
+            gasLimit: 2000000,
+        });
+        await finish_tx.wait(1);
+        log(`Your token URI ${await randomSVG.tokenURI(tokenId)}`);
+    }
 };
 module.exports.tags = ["all", "rsvg"];
